a7c780557c91ddaefeaf67d700cfe19271641bdb
Naufal Alif vivaldi <alifvivaldi726@gmail.com>
2024-09-26 02:50:45 +0700

    2nd commit

--- script.js
+++ script.js
@@ -1,34 +1,3 @@
-
-            const header = document.getElementById('header');
-            const navbar = document.getElementById('navbar');
-            let prevScrollPos = window.pageYOffset;
-
-            window.onscroll = function() {
-                let currentScrollPos = window.pageYOffset;
-
-                // Atur margin-top header saat scroll ke bawah atau ke atas
-                if (currentScrollPos > 50) {
-                    header.classList.remove('mt-20');
-                    header.classList.add('mt-0');
-                    header.classList.remove('bg-transparent');
-                    header.classList.add('bg-coklattua');
-                } else {
-                    header.classList.remove('mt-0');
-                    header.classList.add('mt-20');
-                    header.classList.add('bg-transparent');
-                    header.classList.remove('bg-coklattua');
-                }
-
-                // Sembunyikan navbar jika scroll ke bawah, tampilkan jika scroll ke atas
-                if (prevScrollPos > currentScrollPos) {
-                    navbar.classList.remove('hidden');
-                } else {
-                    navbar.classList.add('hidden');
-                }
-
-                prevScrollPos = currentScrollPos;
-            };
-
 // biar tombol burger bisa diklik
 const menuBtn = document.querySelector('.menu-btn')
 const navlinks =document.querySelector('.nav-links')
@@ -38,12 +7,12 @@ document.querySelector('.menu-btn').addEventListener('click', function() {
 
 menuBtn.addEventListener('click',()=>{
             navlinks.classList.toggle('mobile-menu')
-        })
+        });
 
 // counting
 // Fungsi untuk menghitung naik dari 0 ke angka target
 function countUp() {
-    const counters = document.querySelectorAll('.counter'); // Mengambil semua elemen .counter
+    const counters = document.querySelectorAll('.count'); // Mengambil semua elemen .counter
 
     counters.forEach(counter => {
         const target = +counter.getAttribute('data-count');
